048423bd81848db912561f06c4a42cd47dd894e3
Alexander Hoerl <alexander.hoerl@alexander-productions.de>
2019-03-03 17:35:06 +0100

    added pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+module.exports = {
+  apps : [{
+    name: 'instagram-schedule',
+    script: 'src/index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    env: {
+      NODE_ENV: 'production',
+    },
+  }],
+};
