65d9d410c0ff7bc884bb9657b5fa917238d2a1ee
Alexander Hörl <alexander.horl2@live.de>
2020-10-04 12:30:20 +0200

    cahnged name and env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,20 @@
 /* eslint-disable */
 module.exports = {
-  apps : [{
-    name: 'Instagram Schedule',
-    script: 'src',
+  apps: [
+    {
+      name: 'Instagram Scheduler',
+      script: 'src',
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    ignore_watch: ['cookies', 'uploads'],
-    env: {
-      NODE_ENV: 'production',
-      PORT: 4480,
-      SECRET: '',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      ignore_watch: ['cookies', 'uploads'],
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 4480,
+        SECRET: '',
+      },
     },
-  }],
+  ],
 };
